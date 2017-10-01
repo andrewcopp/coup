dcb352aed4780b6a9bfc41545638d8557d39f380
Andrew Copp <andrew.w.copp@gmail.com>
2017-09-30 22:03:09 -0400

    Fixed Output

--- claim.go
+++ claim.go
@@ -23,16 +23,19 @@ func (c *Claim) Verify(state *State) {
 	revealed := c.Subject.Reveal(state, &c.Declared)
 	c.Challenge.Revealed = revealed.Type
 
+	var loser *Player
 	var discarded *Card
 	if c.Declared != c.Challenge.Revealed {
+		fmt.Printf("Challenge successful.\n")
+		loser = c.Subject
+		discarded = revealed
+	} else {
 		fmt.Printf("Challenge unsuccessful.\n")
+		loser = challenger
 		discarded = challenger.Reveal(state, nil)
 		state.Deck.Add(revealed)
 		c.Subject.Hand = append(c.Subject.Hand, state.Deck.Draw())
-	} else {
-		fmt.Printf("Challenge successful.\n")
-		discarded = revealed
 	}
 
-	fmt.Printf("%s discards a %s.\n", c.Subject.Name, discarded.Name())
+	fmt.Printf("%s discards a %s.\n", loser.Name, discarded.Name())
 }

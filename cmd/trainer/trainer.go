833a4e956244d05b59f8c05a03ef4878a23134e3
Andrew Copp <andrew.w.copp@gmail.com>
2017-10-31 10:17:01 -0400

    Actual Training Parameters

--- cmd/trainer/trainer.go
+++ cmd/trainer/trainer.go
@@ -15,7 +15,7 @@ func init() {
 
 func main() {
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 10; i++ {
 
 		if i != 0 {
 			infile := fmt.Sprintf("./cmd/trainer/models/model_%d.cptk", i)
@@ -33,9 +33,9 @@ func main() {
 		wins := 0
 		losses := 0
 		var chooser coup.Chooser
-		chooser = coup.NewAgent(i, 0.8)
+		chooser = coup.NewAgent(i, 1.0/float64(i))
 		one := coup.NewPlayer("Player One", chooser, false)
-		for j := 0; j < 1; j++ {
+		for j := 0; j < 1000; j++ {
 			rand.Seed(time.Now().UnixNano())
 			if i > 0 {
 				chooser = coup.NewAgent(rand.Intn(i), 0.0)

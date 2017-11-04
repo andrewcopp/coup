f99c72e6082705de736eb166e5b39b5aa3d07689
Andrew Copp <andrew.w.copp@gmail.com>
2017-11-03 21:08:26 -0400

    Removing Second Folder

--- cmd/trainer/model_2/initialize.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from model import Model
-import sys
-
-m = Model()
-m.initialize(sys.argv[1])
--- cmd/trainer/model_2/transfer.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from model import Model
-import sys
-
-m = Model()
-m.transfer(sys.argv[1], sys.argv[2])
